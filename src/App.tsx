e8b717ac1b97affe4220db5eeae1cd18c0f42b5f
Nguyen Duy Truong <truongezgg@Nguyens-MacBook-Pro.local>
2024-07-28 21:08:08 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { TonConnectButton } from "@tonconnect/ui-react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TonConnectButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+const manifestUrl = "/manifest.json";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </React.StrictMode>
+);
