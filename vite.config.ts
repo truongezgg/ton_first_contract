80d7b96521aa030f59db3a572b8335d861cb401c
Nguyen Duy Truong <truongezgg@Nguyens-MacBook-Pro.local>
2024-07-29 11:00:29 +0700

    fix wrong path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/first_ton_contract/",
+  base: "/ton_first_contract",
 });
